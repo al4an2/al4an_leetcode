1fbef5931074ae9f2d5a51618bad559ac2580f04
al4an2 <al4an2@gmail.com>
2026-01-08 23:59:22 +0100

    dp task done

---
+++ tasks/53. Maximum Subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = nums[0]
+        max_sum = nums[0]
+
+        for i in range(1, n):
+            dp = max(nums[i] + dp, nums[i])
+            max_sum = max(max_sum, dp)
+    
+        return max_sum
+            
