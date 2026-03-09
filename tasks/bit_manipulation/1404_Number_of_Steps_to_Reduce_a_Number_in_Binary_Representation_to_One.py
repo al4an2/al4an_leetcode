3d1e4794ae8d845f1189b2c0dd002318a16fa14d
al4an2 <al4an2@gmail.com>
2026-03-09 21:26:50 +0100

    some task done

--- tasks/bit_manipulation/1404_Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One.py
+++ tasks/bit_manipulation/1404_Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One.py
@@ -21,3 +21,21 @@ class Solution:
                 counter += 1
 
         return counter + int(carry)
+    
+
+#gredy(another design)
+
+class Solution:
+    def numSteps(self, s: str) -> int:
+        
+        counter, carry = 0, 0
+        for n in reversed(s[1:]):
+            num = int(n) + carry
+            if num % 2 == 1:
+                
+                counter += 2
+                carry = 1
+            else:
+                counter += 1
+        
+        return counter + carry
