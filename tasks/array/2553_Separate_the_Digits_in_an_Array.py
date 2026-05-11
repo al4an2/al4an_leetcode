5baa52396ee784fa1498f6bff91218d2b8e7fb08
al4an2 <al4an2@gmail.com>
2026-05-11 18:27:13 +0200

    daily task done another way

--- tasks/array/2553_Separate_the_Digits_in_an_Array.py
+++ tasks/array/2553_Separate_the_Digits_in_an_Array.py
@@ -5,3 +5,16 @@ class Solution:
             for x in str(n):
                 ans.append(int(x))
         return ans
+
+#without string manipulation
+class Solution:
+    def separateDigits(self, nums: list[int]) -> list[int]:
+        ans = []
+
+        for i in range(len(nums)-1, -1, -1):
+            val = nums[i]
+            while val > 0:
+                ans.append(val % 10)
+                val = val // 10
+        ans.reverse()
+        return ans
